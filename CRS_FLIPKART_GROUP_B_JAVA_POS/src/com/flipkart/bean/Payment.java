a234b35778d4da713829e3c46eede9715a6debd8
sanjeev-flipKart <sanjeev.mallick@flipkart.com>
2024-08-07 22:58:59 +0530

    Completed ProfessorOperation.java

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/Payment.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/Payment.java
@@ -1,72 +1,57 @@
 package com.flipkart.bean;
-import java.util.Date;
 
 public class Payment {
+    private String paymentID;
+    private int studentID;
+    private int semester;
+    private double amount;
+    private boolean paymentStatus;
+    private String paymentDate;
+    
+    public String getPaymentID() {
+        return paymentID;
+    }
+    public int getStudentID() {
+        return studentID;
+    }
+    public int getSemester() {
+        return semester;
+    }
+    public double getPaymentAmount()
+    {
+        return amount;
+    }
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+    public boolean getPaymentStatus() {
+        return paymentStatus;
+    }
+    public void setPaymentID(String paymentID) {
+        this.paymentID = paymentID;
+    }
+  
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+   
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+    public void setPaymentAmount(double amount) {
+        this.amount = amount;
+    }
+    
+    public void setPaymentStatus(boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+    
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+    public void makePayment()
+    {
+        
+    }
 
-	private String paymentID;
-	private int studentID;
-	private int semester;
-	private double amount;
-	private boolean paymentStatus;
-	private Date paymentDate;
-	
-	public Payment(String paymentID, int studentID, int semester, double amount, boolean paymentStatus,
-			Date paymentDate) {
-		
-		this.paymentID = paymentID;
-		this.studentID = studentID;
-		this.semester = semester;
-		this.amount = amount;
-		this.paymentStatus = paymentStatus;
-		this.paymentDate = paymentDate;
-	}
-	
-	
-	public String getPaymentID() {
-		return paymentID;
-	}
-
-	public void setPaymentID(String paymentID) {
-		this.paymentID = paymentID;
-	}
-
-	public int getStudentID() {
-		return studentID;
-	}
-
-	public void setStudentID(int studentID) {
-		this.studentID = studentID;
-	}
-
-	public int getSemester() {
-		return semester;
-	}
-
-	public void setSemester(int semester) {
-		this.semester = semester;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public boolean isPaymentStatus() {
-		return paymentStatus;
-	}
-
-	public void setPaymentStatus(boolean paymentStatus) {
-		this.paymentStatus = paymentStatus;
-	}
-
-	public Date getPaymentDate() {
-		return paymentDate;
-	}
-
-	public void setPaymentDate(Date paymentDate) {
-		this.paymentDate = paymentDate;
-	}
 }
--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/business/ProfessorOperation.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/business/ProfessorOperation.java
@@ -2,26 +2,48 @@ package com.flipkart.business;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.RegisteredCourses;
+import com.flipkart.bean.Student;
 
 
 @SuppressWarnings("unused")
 public class ProfessorOperation{
 
-	
+	private String name;
+    private String email;
+    private String phoneNo;
 
-	public void addGrade(Integer studentID, Integer semesterID, String courseID, Integer grade)  
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void recordGrade(Student student, Course course, String grade)  
 	{
 		
 	}
 
-	public void viewCourseStudents(String courseID, Integer semesterID) {
+	public void viewEnrolledStudents(String courseID, Integer semesterID) {
 		
 			
-	}
-
-	
-	public void viewCourseProf(int instructorID) {
-		
 	}
 
 	public void registerCourse(int instructorID, Integer semesterID, String courseID) {
