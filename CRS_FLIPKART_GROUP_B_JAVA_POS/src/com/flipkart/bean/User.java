713cf3b8d3ce6c37ffb63415f3c8f72e0ed642ff
yashKKFlipkart <yash.kumarkandoi@flipkart.com>
2024-08-07 15:24:02 +0530

    Removed Customer.java and Added User.java Files

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/User.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/User.java
@@ -7,5 +7,59 @@ package com.flipkart.bean;
  * 
  */
 public class User {
+	
+	private int userID;
+	private String name;
+	private String role;
+	private String password;
+	private String joiningYear;
+	
+	
+	
+	public User(int userID) {
+		this.userID = userID;
+	}
+	
+	
+	public User(int userID, String name, String role, String password, String joiningYear) {
+		this.userID = userID;
+		this.name = name;
+		this.role = role;
+		this.password = password;
+		this.joiningYear = joiningYear;
+	}
 
+
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getJoiningYear() {
+		return joiningYear;
+	}
+	public void setJoiningYear(String joiningYear) {
+		this.joiningYear = joiningYear;
+	}
+	
+	
 }
