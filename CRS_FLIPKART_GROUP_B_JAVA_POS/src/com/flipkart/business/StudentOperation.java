6a105dd7afffb970c2beb65590b20f82f2dfb62b
yashKKFlipkart <yash.kumarkandoi@flipkart.com>
2024-08-07 22:58:27 +0530

    Modifications to StudentOperations.java

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/business/StudentOperation.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/business/StudentOperation.java
@@ -1,5 +1,22 @@
 package com.flipkart.business;
 
+import java.util.List;
+
+import com.flipkart.bean.Course;
+
 public class StudentOperation {
     
+	public void register() {
+		
+	}
+	
+	public List<Course> viewRegisteredCourses(){
+		return null;
+		
+	}
+	
+	public void viewReportCard() {
+		
+	}
+	
 }
