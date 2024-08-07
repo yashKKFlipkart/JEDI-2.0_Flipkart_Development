cbc55b023b19e88df8e845c262d8d7e848cd7af2
yashKKFlipkart <yash.kumarkandoi@flipkart.com>
2024-08-07 22:53:37 +0530

    Commit 14

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/Payment.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/bean/Payment.java
@@ -2,14 +2,7 @@ package com.flipkart.bean;
 import java.util.Date;
 
 public class Payment {
-    private String paymentID;
-    private int studentID;
-    private int semester;
-    private double amount;
-    private boolean paymentStatus;
-    private String paymentDate;
-    
-<<<<<<< Updated upstream
+
 	private String paymentID;
 	private int studentID;
 	private int semester;
@@ -76,58 +69,4 @@ public class Payment {
 	public void setPaymentDate(Date paymentDate) {
 		this.paymentDate = paymentDate;
 	}
-
-	
-	
-	
-	
-	
-=======
-    public String getPaymentID() {
-        return paymentID;
-    }
-    public int getStudentID() {
-        return studentID;
-    }
-    public int getSemester() {
-        return semester;
-    }
-    public double getPaymentAmount()
-    {
-        return amount;
-    }
-    public String getPaymentDate() {
-        return paymentDate;
-    }
-    public boolean getPaymentStatus() {
-        return paymentStatus;
-    }
-    public void setPaymentID(String paymentID) {
-        this.paymentID = paymentID;
-    }
-  
-    public void setStudentID(int studentID) {
-        this.studentID = studentID;
-    }
-   
-    public void setSemester(int semester) {
-        this.semester = semester;
-    }
-    public void setPaymentAmount(double amount) {
-        this.amount = amount;
-    }
-    
-    public void setPaymentStatus(boolean paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-    
-    public void setPaymentDate(String paymentDate) {
-        this.paymentDate = paymentDate;
-    }
-    public void makePayment()
-    {
-        
-    }
-
->>>>>>> Stashed changes
 }
