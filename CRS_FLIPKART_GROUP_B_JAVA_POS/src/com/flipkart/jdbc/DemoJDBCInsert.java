58f9ccbd1a6af63f308d73d1951591bee2a57c64
sanjeev-flipKart <sanjeev.mallick@flipkart.com>
2024-08-11 22:25:04 +0530

    Added objects in TABLE

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/jdbc/DemoJDBCInsert.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/jdbc/DemoJDBCInsert.java
@@ -1,24 +1,27 @@
 package com.flipkart.jdbc;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class DemoJDBCInsert {
-	public static void main(String args[])
-	{ 
-		try{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo_flipfit","root","sanjeev-flipkart");
-		PreparedStatement stmt=con.prepareStatement("insert into employee values(?,?,?)");											
-		stmt.setInt(1,101);//1 specifies the first parameter in the query
-		stmt.setString(2,"Ratan"); stmt.setString(3, "Delhi"); int
-		i=stmt.executeUpdate(); System.out.println(i+" records inserted");
-		con.close(); }
-		catch(Exception e)
-		{
-		System.out.println(e);}
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo_flipfit", "root",
+					"sanjeev-flipkart");
+			PreparedStatement stmt = con.prepareStatement("insert into employee values(?,?,?)");
+			stmt.setInt(1, 101);// 1 specifies the first parameter in the query
+			stmt.setString(2, "Ratan");
+			stmt.setString(3, "Delhi");
+			stmt.setInt(1, 102);// 1 specifies the first parameter in the query
+			stmt.setString(2, "Mohan");
+			stmt.setString(3, "Mumbai");
+			int i = stmt.executeUpdate();
+			System.out.println(i + " records inserted");
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
-
