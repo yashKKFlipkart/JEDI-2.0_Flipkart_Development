a9b5a0431b05955940ed31483eea433254e2721a
yashKKFlipkart <yash.kumarkandoi@flipkart.com>
2024-08-13 11:19:53 +0530

    Comments added

--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/dao/ProfessorDAOInterface.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/dao/ProfessorDAOInterface.java
@@ -2,23 +2,62 @@ package com.flipkart.dao;
 
 import com.flipkart.bean.Professor;
 
+/**
+ * Interface representing the data access object (DAO) for professor-related operations.
+ * Provides methods for professors to manage grades, view their courses, and other tasks.
+ */
 public interface ProfessorDAOInterface {
 
-	public void addGrade(Integer studentID,Integer courseID, String alphaGrade);
-	
-	// view enrolled students
-	public void ViewEnrolledStudents(Integer courseID);
-	
-	// view courses signed by by the professor
-	public void viewSignedUpCourses(int instructorID) ;
-	
-	// sign up for course
-	public void registerCourse(int instructorID, String courseName, Integer courseID);
-	
-	public Professor findProfessorByUsername(String username);
-	
-	public Professor findProfessorById(Integer instructorID);
+    /**
+     * Adds a grade for a student in a specific course.
+     * 
+     * @param studentID Unique identifier of the student.
+     * @param courseID Unique identifier of the course.
+     * @param alphaGrade The grade in alphabetic format (e.g., A, B, C).
+     */
+    public void addGrade(Integer studentID, Integer courseID, String alphaGrade);
 
-	public void viewProfessors();
-	
+    /**
+     * Displays a list of students enrolled in a specific course.
+     * 
+     * @param courseID Unique identifier of the course.
+     */
+    public void ViewEnrolledStudents(Integer courseID);
+
+    /**
+     * Displays the courses that a professor has signed up for.
+     * 
+     * @param instructorID Unique identifier of the professor.
+     */
+    public void viewSignedUpCourses(int instructorID);
+
+    /**
+     * Allows a professor to sign up for a course.
+     * 
+     * @param instructorID Unique identifier of the professor.
+     * @param courseName Name of the course.
+     * @param courseID Unique identifier of the course.
+     */
+    public void registerCourse(int instructorID, String courseName, Integer courseID);
+
+    /**
+     * Finds a professor by their username.
+     * 
+     * @param username Username of the professor.
+     * @return Professor object corresponding to the username.
+     */
+    public Professor findProfessorByUsername(String username);
+
+    /**
+     * Finds a professor by their unique identifier.
+     * 
+     * @param instructorID Unique identifier of the professor.
+     * @return Professor object corresponding to the instructor ID.
+     */
+    public Professor findProfessorById(Integer instructorID);
+
+    /**
+     * Displays a list of all professors.
+     */
+    public void viewProfessors();
 }
--- CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/dao/UserDAOInterface.java
+++ CRS_FLIPKART_GROUP_B_JAVA_POS/src/com/flipkart/dao/UserDAOInterface.java
@@ -1,13 +1,45 @@
 package com.flipkart.dao;
 
+/**
+ * This interface defines the contract for user-related database operations.
+ * Implementing classes should provide the necessary logic to interact with the
+ * user database, including password updates and login functionality for
+ * different user roles.
+ */
 public interface UserDAOInterface {
 
-	public void updateProfessorPassword(Integer userID, String password);
-	 
-	public void updateAdminPassword(Integer userID, String password);
-	
-	public void updateStudentPassword(Integer userID, String password);
-	
-	public boolean loginUser(Integer userID, String password, String role);
-	
+    /**
+     * Updates the password for a professor in the system.
+     * 
+     * @param userID   The unique identifier of the professor.
+     * @param password The new password to set for the professor.
+     */
+    public void updateProfessorPassword(Integer userID, String password);
+     
+    /**
+     * Updates the password for an admin in the system.
+     * 
+     * @param userID   The unique identifier of the admin.
+     * @param password The new password to set for the admin.
+     */
+    public void updateAdminPassword(Integer userID, String password);
+    
+    /**
+     * Updates the password for a student in the system.
+     * 
+     * @param userID   The unique identifier of the student.
+     * @param password The new password to set for the student.
+     */
+    public void updateStudentPassword(Integer userID, String password);
+    
+    /**
+     * Authenticates a user in the system based on their role.
+     * 
+     * @param userID   The unique identifier of the user.
+     * @param password The password entered by the user.
+     * @param role     The role of the user (e.g., professor, admin, student).
+     * @return         True if the login is successful, false otherwise.
+     */
+    public boolean loginUser(Integer userID, String password, String role);
+    
 }
